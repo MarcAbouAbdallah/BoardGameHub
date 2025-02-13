def82cbbc17055cc4f2726e21ef3c84b1dd37da8
Bora Denizasan <umit.denizasan@mail.mcgill.ca>
2025-02-12 21:58:07 -0500

    RegistirationRepoTest

--- BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/repo/RegistirationRepositoryTests.java
+++ BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/repo/RegistirationRepositoryTests.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse321.boardgamehub.repo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +22,14 @@ public class RegistirationRepositoryTests {
     @Autowired
     private EventRepository eventRepo;
 
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+        registrationRepo.deleteAll();
+        personRepo.deleteAll();
+        eventRepo.deleteAll();
+    }
+
     @Test
     public void testCreateAndReadRegistration() {
         //Arrange
@@ -27,11 +37,30 @@ public class RegistirationRepositoryTests {
                                  "john@gmail.com",
                                  "John@123",
                                  true);
+        john = personRepo.save(john);
 
-        Registration registration = new Registration(john,);
+        Event hanging = new Event("hanging",
+                                  null, 
+                                  null, 
+                                  null, 
+                                  1, 
+                                  john, 
+                                  null);
+        hanging = eventRepo.save(hanging);
+
+        Registration.Key key = new Registration.Key(john, hanging);
+        Registration registration = new Registration(key);
         registration = registrationRepo.save(registration);
 
         //Act
-        Registration RegistrationFromDb = registrationRepo.findRegistrationByKey(registration.getKey());
+        Registration RegistrationFromDb = registrationRepo.findRegistrationByKey(key);
+
+        //Assert
+        assertNotNull(RegistrationFromDb);
+        assertNotNull(RegistrationFromDb.getKey());
+        assertNotNull(RegistrationFromDb.getKey().getRegistrant().getId());
+        assertEquals(john.getId(), RegistrationFromDb.getKey().getRegistrant().getId());
+        assertNotNull(RegistrationFromDb.getKey().getRegisteredEvent());
+        assertEquals(hanging.getId(), RegistrationFromDb.getKey().getRegisteredEvent().getId());
 }
 }
