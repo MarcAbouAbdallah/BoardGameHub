aecbc483f3be8828b6dba0e7d04f95185f54aaaa
JeromeCourtemanche <jerome9440@gmail.com>
2025-02-16 09:54:12 -0500

    Added review tests, review search options & test logging.

--- BoardGameHub-Backend/src/main/java/ca/mcgill/ecse321/boardgamehub/repo/ReviewRepository.java
+++ BoardGameHub-Backend/src/main/java/ca/mcgill/ecse321/boardgamehub/repo/ReviewRepository.java
@@ -2,7 +2,16 @@ package ca.mcgill.ecse321.boardgamehub.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.boardgamehub.model.Review;
+import ca.mcgill.ecse321.boardgamehub.model.Player;
+import ca.mcgill.ecse321.boardgamehub.model.Game;
+
+
+import java.util.List;
+
 
 public interface ReviewRepository extends CrudRepository<Review, Integer> {
     public Review findReviewById(int id);
+    public List<Review> findByReviewer(Player reviewer);
+    public List<Review> findByGame(Game game);
+    public List<Review> findByReviewerAndGame(Player reviewer, Game game);
 }
--- BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/repo/ReviewRepositoryTests.java
+++ BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/repo/ReviewRepositoryTests.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse321.boardgamehub.repo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import java.sql.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -74,4 +78,190 @@ public class ReviewRepositoryTests {
 		assertEquals(monopolyReview.getReviewer().getId(), monopolyReviewFromDb.getReviewer().getId()); // For this and next test, unpredictable behaviour of assertequals on objects,
         assertEquals(monopolyReview.getGame().getId(), monopolyReviewFromDb.getGame().getId());     // asserted Id instead. What do?
     }
+
+    @Test
+    public void testUpdateReview() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        monopolyReview.setComment("My friends hate me now");
+        monopolyReview.setRating(1);
+        monopolyReview.setDate(Date.valueOf("2025-02-15")); 
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        //Act
+        Review monopolyReviewFromDb = reviewRepo.findReviewById(monopolyReview.getId());
+
+        //Assert
+        assertNotNull(monopolyReviewFromDb);
+        assertEquals(monopolyReview.getId(), monopolyReviewFromDb.getId());
+		assertEquals(monopolyReview.getRating(), monopolyReviewFromDb.getRating());
+		assertEquals(monopolyReview.getComment(), monopolyReviewFromDb.getComment());
+		assertEquals(monopolyReview.getDate(), monopolyReviewFromDb.getDate());
+		assertEquals(monopolyReview.getReviewer().getId(), monopolyReviewFromDb.getReviewer().getId());
+        assertEquals(monopolyReview.getGame().getId(), monopolyReviewFromDb.getGame().getId());
+    }
+
+    @Test
+    public void deleteReview() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        //Act
+        reviewRepo.delete(monopolyReview);
+
+        //Assert
+        Review monopolyReviewFromDb = reviewRepo.findReviewById(monopolyReview.getId());
+        assertNull(monopolyReviewFromDb);
+    }
+
+    @Test
+    public void nonExistentReview() {
+        //Arrange
+        //(done in before each)
+
+        //Act
+        List<Review> fakeMonopolyReviewFromDb = reviewRepo.findByReviewerAndGame(john, monopoly);
+
+        //Assert
+        assertEquals(0, fakeMonopolyReviewFromDb.size());
+    }
+
+    @Test
+    public void findByPlayer() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        //Act
+        List<Review> playerReviews = reviewRepo.findByReviewer(john);
+
+        //Assert
+        assertFalse(playerReviews.isEmpty());
+        assertEquals(1, playerReviews.size());
+
+        Review monopolyReviewFromDb = playerReviews.get(0);
+
+        assertNotNull(monopolyReviewFromDb);
+        assertEquals(monopolyReview.getId(), monopolyReviewFromDb.getId());
+		assertEquals(monopolyReview.getRating(), monopolyReviewFromDb.getRating());
+		assertEquals(monopolyReview.getComment(), monopolyReviewFromDb.getComment());
+		assertEquals(monopolyReview.getDate(), monopolyReviewFromDb.getDate());
+		assertEquals(monopolyReview.getReviewer().getId(), monopolyReviewFromDb.getReviewer().getId());
+        assertEquals(monopolyReview.getGame().getId(), monopolyReviewFromDb.getGame().getId());
+    }
+
+    @Test
+    public void findByGame() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        //Act
+        List<Review> gameReviews = reviewRepo.findByGame(monopoly);
+
+        //Assert
+        assertFalse(gameReviews.isEmpty());
+        assertEquals(1, gameReviews.size());
+
+        Review monopolyReviewFromDb = gameReviews.get(0);
+
+        assertNotNull(monopolyReviewFromDb);
+        assertEquals(monopolyReview.getId(), monopolyReviewFromDb.getId());
+		assertEquals(monopolyReview.getRating(), monopolyReviewFromDb.getRating());
+		assertEquals(monopolyReview.getComment(), monopolyReviewFromDb.getComment());
+		assertEquals(monopolyReview.getDate(), monopolyReviewFromDb.getDate());
+		assertEquals(monopolyReview.getReviewer().getId(), monopolyReviewFromDb.getReviewer().getId());
+        assertEquals(monopolyReview.getGame().getId(), monopolyReviewFromDb.getGame().getId());
+    }
+
+    @Test
+    public void findByPlayerAndGame() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        //Act
+        List<Review> playerGameReviews = reviewRepo.findByReviewerAndGame(john, monopoly);
+
+        //Assert
+        assertFalse(playerGameReviews.isEmpty());
+        assertEquals(1, playerGameReviews.size());
+
+        Review monopolyReviewFromDb = playerGameReviews.get(0);
+
+        assertNotNull(monopolyReviewFromDb);
+        assertEquals(monopolyReview.getId(), monopolyReviewFromDb.getId());
+		assertEquals(monopolyReview.getRating(), monopolyReviewFromDb.getRating());
+		assertEquals(monopolyReview.getComment(), monopolyReviewFromDb.getComment());
+		assertEquals(monopolyReview.getDate(), monopolyReviewFromDb.getDate());
+		assertEquals(monopolyReview.getReviewer().getId(), monopolyReviewFromDb.getReviewer().getId());
+        assertEquals(monopolyReview.getGame().getId(), monopolyReviewFromDb.getGame().getId());
+    }
+
+    @Test
+    public void multipleReviews() {
+        //Arrange
+        Date today = Date.valueOf("2025-02-07");
+        Review monopolyReview = new Review(
+                                    9,
+                                    "I won, but at what cost?",
+                                    today,
+                                    john,
+                                    monopoly);
+        monopolyReview = reviewRepo.save(monopolyReview);
+
+        Date yesterday = Date.valueOf("2025-02-06");
+        Review monopolyReviewSecond = new Review(
+                                    2,
+                                    "I lost this time, which definitely means it's a bad game...",
+                                    yesterday,
+                                    john,
+                                    monopoly);
+        monopolyReviewSecond = reviewRepo.save(monopolyReviewSecond);
+
+        //Act
+        List<Review> reviewsFromDb = reviewRepo.findByReviewerAndGame(john, monopoly);
+
+        //Assert
+        assertEquals(2, reviewsFromDb.size());
+    }
+
 }
