026e247d5b7282205dbe9fcf98bb1bc6bdd6a998
Deon Aftahi <deon.aftahi@gmail.com>
2025-03-08 17:01:02 -0500

    updated GameCopyRepository

--- BoardGameHub-Backend/src/main/java/ca/mcgill/ecse321/boardgamehub/repo/GameCopyRepository.java
+++ BoardGameHub-Backend/src/main/java/ca/mcgill/ecse321/boardgamehub/repo/GameCopyRepository.java
@@ -1,8 +1,15 @@
 package ca.mcgill.ecse321.boardgamehub.repo;
 
 import ca.mcgill.ecse321.boardgamehub.model.GameCopy;
+import ca.mcgill.ecse321.boardgamehub.model.Player;
+import ca.mcgill.ecse321.boardgamehub.model.Game;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface GameCopyRepository extends CrudRepository<GameCopy, Integer>{
     public GameCopy findGameCopyById(int id);
+    public List<GameCopy> findByOwner(Player owner);
+    public List<GameCopy> findByGame(Game game);
 }
