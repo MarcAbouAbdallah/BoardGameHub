d9f88b1dac540236fed9051637f4132c59eb7b5a
Bora Denizasan <umit.denizasan@mail.mcgill.ca>
2025-03-17 22:45:03 -0400

    This one is the tests

--- BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/integration/RegistrationIntegrationTest.java
+++ BoardGameHub-Backend/src/test/java/ca/mcgill/ecse321/boardgamehub/integration/RegistrationIntegrationTest.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.boardgamehub.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Date;
@@ -11,9 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
