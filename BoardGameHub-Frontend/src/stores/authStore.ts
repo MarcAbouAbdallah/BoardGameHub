a9ffe499b3dc172f907d898a55bec0ad85dfaba8
Mubeen Mohammed <mubeen.mohammed@mail.mcgill.ca>
2025-04-03 19:15:16 -0400

    added ui for profile sheet (#222)

--- BoardGameHub-Frontend/src/stores/authStore.ts
+++ BoardGameHub-Frontend/src/stores/authStore.ts
@@ -1,46 +1,54 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
 interface User {
-    username: string | null;
-    userEmail: string | null;
-    userId: number | null;
-    isAuthenticated: boolean;
+  username: string | null;
+  userEmail: string | null;
+  userId: number | null;
+  isAuthenticated: boolean;
+}
+
+export const useAuthStore = defineStore("auth", () => {
+  const user = ref<User>({
+    username: null,
+    userEmail: null,
+    userId: null,
+    isAuthenticated: false,
+  });
+
+  const storedUser = localStorage.getItem("user");
+  if (storedUser) {
+    user.value = JSON.parse(storedUser);
   }
 
-export const useAuthStore = defineStore('auth', () => {
-    const user = ref<User>(
-        {
-            username: null,
-            userEmail: null,
-            userId: null,
-            isAuthenticated: false,
-        });
+  function login(name: string, email: string, id: number, pwd: string) {
+    if (!pwd) return;
+    user.value.username = name;
+    user.value.userEmail = email;
+    user.value.userId = id;
+    user.value.isAuthenticated = true;
 
-        const storedUser = localStorage.getItem("user");
-        if (storedUser) {
-            user.value = JSON.parse(storedUser);
+    localStorage.setItem("user", JSON.stringify(user.value));
   }
-    
 
-    function login(name: string, email: string, id: number, pwd: string) {
-        if (!pwd) return;
-        user.value.username = name;
-        user.value.userEmail = email;
-        user.value.userId = id;
-        user.value.isAuthenticated = true;
+  function logout() {
+    user.value.username = null;
+    user.value.userEmail = null;
+    user.value.userId = null;
+    user.value.isAuthenticated = false;
 
-        localStorage.setItem("user", JSON.stringify(user.value));
-    }
+    localStorage.removeItem("user");
+  }
 
-    function logout() {
-        user.value.username = null;
-        user.value.userEmail = null;
-        user.value.userId = null;
-        user.value.isAuthenticated = false;
+  function changeUsername(newUsername: string) {
+    user.value.username = newUsername;
+    localStorage.setItem("user", JSON.stringify(user.value));
+  }
 
-        localStorage.removeItem("user");
-    }
+  function changeUserEmail(newUserEmail: string) {
+    user.value.userEmail = newUserEmail;
+    localStorage.setItem("user", JSON.stringify(user.value));
+  }
 
-    return { user, login, logout }
-})
+  return { user, login, logout, changeUsername, changeUserEmail };
+});
