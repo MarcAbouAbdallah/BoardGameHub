24b8baa780a4b86277037b8386e935264d0334f5
Bora Denizasan <umit.denizasan@mail.mcgill.ca>
2025-04-04 17:49:28 -0400

    can be used to check if someone is gameOwne, also added a function to change the gameOwner status that needs to be put in after addGame_copy and remove_gamecopy

--- BoardGameHub-Frontend/src/stores/authStore.ts
+++ BoardGameHub-Frontend/src/stores/authStore.ts
@@ -6,6 +6,7 @@ interface User {
   userEmail: string | null;
   userId: number | null;
   userPassword: string | null;
+  isGameOwner: boolean;
   isAuthenticated: boolean;
 }
 
@@ -15,6 +16,7 @@ export const useAuthStore = defineStore("auth", () => {
     userEmail: null,
     userId: null,
     userPassword: null,
+    isGameOwner: false,
     isAuthenticated: false,
   });
 
@@ -23,12 +25,13 @@ export const useAuthStore = defineStore("auth", () => {
     user.value = JSON.parse(storedUser);
   }
 
-  function login(name: string, email: string, id: number, pwd: string) {
+  function login(name: string, email: string, id: number, pwd: string, gameOwner: boolean) {
     if (!pwd) return;
     user.value.username = name;
     user.value.userEmail = email;
     user.value.userId = id;
     user.value.userPassword = pwd;
+    user.value.isGameOwner = gameOwner;
     user.value.isAuthenticated = true;
 
     localStorage.setItem("user", JSON.stringify(user.value));
@@ -39,6 +42,7 @@ export const useAuthStore = defineStore("auth", () => {
     user.value.userEmail = null;
     user.value.userId = null;
     user.value.userPassword = null;
+    user.value.isGameOwner = false;
     user.value.isAuthenticated = false;
     localStorage.removeItem("user");
   }
@@ -58,5 +62,15 @@ export const useAuthStore = defineStore("auth", () => {
     localStorage.setItem("user", JSON.stringify(user.value));
   }
 
-  return { user, login, logout, changeUsername, changeUserEmail, changeUserPassword };
+  function changeGameOwnerStatus() {
+    if (user.value.isGameOwner = true) {
+      user.value.isGameOwner = false;
+    }
+    else {
+      user.value.isGameOwner = true;
+    }
+    localStorage.setItem("user", JSON.stringify(user.value));
+  }
+
+  return { user, login, logout, changeUsername, changeUserEmail, changeUserPassword, changeGameOwnerStatus };
 });
