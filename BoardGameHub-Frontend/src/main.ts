5c0dea969b921ae4f618c7137be7f6b699f763c0
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2025-04-04 21:01:06 -0400

    fix/dep_error

--- BoardGameHub-Frontend/src/main.ts
+++ BoardGameHub-Frontend/src/main.ts
@@ -3,15 +3,9 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import PrimeVue from "primevue/config";
-import ToastService from "primevue/toastservice";
-import ConfirmationService from "primevue/confirmationservice";
 
 const app = createApp(App);
 app.use(createPinia());
 
 app.use(router);
-app.use(PrimeVue, { unstyled: true });
-app.use(ToastService);
-app.use(ConfirmationService);
 app.mount("#app");
